37719c1f31d9b3b9950109f600bfda46847c14d2
gpelincel <gabrielpelincel@gmail.com>
2025-04-24 15:37:54 -0300

    Feat: Tabela de histórico

--- Frontend/src/app/components/historico/historico.component.ts
+++ Frontend/src/app/components/historico/historico.component.ts
@@ -1,11 +1,20 @@
+import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-historico',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './historico.component.html',
   styleUrl: './historico.component.scss'
 })
 export class HistoricoComponent {
-
+  dadosConsumo: any[] = [
+    { dia: '18/04', consumo: 12.5, valor: 8.75 },
+    { dia: '19/04', consumo: 10.8, valor: 7.56 },
+    { dia: '20/04', consumo: 11.3, valor: 7.91 },
+    { dia: '21/04', consumo: 13.0, valor: 9.10 },
+    { dia: '22/04', consumo: 9.7, valor: 6.79 },
+    { dia: '23/04', consumo: 12.1, valor: 8.47 },
+    { dia: '24/04', consumo: 11.6, valor: 8.12 },
+  ];
 }
--- Frontend/src/app/pages/dashboard/dashboard.component.ts
+++ Frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { VisaoGeralComponent } from '../../components/visao-geral/visao-geral.component';
 import { GraficoDonutComponent } from '../../components/grafico-donut/grafico-donut.component';
 import { LineChartComponent } from '../../components/line-chart/line-chart.component';
+import { HistoricoComponent } from '../../components/historico/historico.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [VisaoGeralComponent, GraficoDonutComponent, LineChartComponent],
+  imports: [VisaoGeralComponent, GraficoDonutComponent, LineChartComponent, HistoricoComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
