2f99770ba25d49c93e6db4396101e3cbade6b839
KaiqueVA <kaique_x_@hotmail.com>
2025-05-04 17:45:01 -0300

    feat: add UART ESP simulator with commands and data processing

---
+++ Hardware/Firmware/energy_management/Testes/simulador_UART_ESP/simuladorESP.py
@@ -0,0 +1,132 @@
+import serial
+import time
+import requests
+import os
+
+# === CONFIGURAÇÃO SERIAL E HTTP ===
+COM_PORT = 'COM3'          # Altere para sua porta
+BAUD_RATE = 115200
+LOGIN_ENDPOINT = "http://192.168.15.157:8080/auth/login"
+SEND_ENDPOINT = ""
+
+payload = {
+    "email": "lelezinho@email.com",
+    "password": "266468"
+}
+
+
+# === COMANDOS AT FIXOS ===
+FIXED_COMMANDS = {
+    "AT": "OK",
+    "AT+INIT": "OK",
+    "AT+SEND": "OK",
+    "AT+SETUPWIFI": "OK",
+    "AT+RESET": "OK",
+}
+
+FIXED_COMMANDS_ESP = {
+    "AT+RELE_ON": "OK",
+    "AT+RELE_OFF": "OK",
+}
+
+# === COMANDOS COM VALOR ===
+ARG_COMMANDS = {
+    "AT+VEFICAZ=": "vEficaz",
+    "AT+IEFICAZ=": "iEficaz",
+    "AT+PAPARENTE=": "pAparente",
+    "AT+PATIVA=": "pAtiva",
+    "AT+PREATIVA=": "pReativa",
+    "AT+FP=": "fatorPotencia"
+}
+
+values = {
+    "vEficaz": 0.0,
+    "iEficaz": 0.0,
+    "pAparente": 0.0,
+    "pAtiva": 0.0,
+    "pReativa": 0.0,
+    "fatorPotencia": 0.0
+}
+
+# === PARSE DE COMANDOS ===
+def parse_command(cmd):
+    cmd = cmd.strip().replace('\r', '').replace('\n', '')
+    if cmd.startswith("AT+DATA"):
+        return cmd
+    
+    if cmd in FIXED_COMMANDS:
+        return FIXED_COMMANDS[cmd]
+    for prefix, field in ARG_COMMANDS.items():
+        if cmd.startswith(prefix):
+            try:
+                value = float(cmd[len(prefix):])
+                energia_data[field] = value
+                return "OK"
+            except:
+                return "NO"
+
+    return "NO"
+
+# === LOOP PRINCIPAL ===
+def simulate_esp():
+    try:
+        response = requests.post(LOGIN_ENDPOINT, json=payload)
+        if response.status_code == 200:
+            print("Login realizado com sucesso!")
+            print("Token ou resposta:", response.json())
+        else:
+            print(f"Erro ao fazer login: {response.status_code}")
+            print(response.text)
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Erro na requisição: {e}")
+        
+
+    
+    try:
+        with serial.Serial(COM_PORT, BAUD_RATE, timeout=1) as ser:
+            print(f"Simulador AT em {COM_PORT} @ {BAUD_RATE}")
+            while True:
+                if ser.in_waiting:
+                    data = ser.readline().decode(errors='ignore')
+                    response = parse_command(data)
+                    if response.startswith("AT+DATA_"):
+                        try:
+                            keys = list(values.keys())  # ['vEficaz', 'iEficaz', ..., 'fatorPotencia']
+                            for i in range(6):
+                                if i == 0:
+                                    data_line = data.strip()
+                                else:
+                                    data_line = ser.readline().decode(errors='ignore').strip()
+
+                                print(f"Linha recebida: {data_line}")
+
+                                # Agora corretamente alinhado
+                                parts = data_line.split('=')
+                                if len(parts) == 2:
+                                    valor_recebido = float(parts[1])
+                                    values[keys[i]] = valor_recebido
+                                else:
+                                    print(f"Formato inválido em: {data_line}")
+
+                            print("\nValores atualizados:")
+                            for key, val in values.items():
+                                print(f"    {key}: {val}")
+
+                            #ser.write(("OK\r\n").encode())
+
+                        except Exception as e:
+                            print(f"Erro no processamento dos valores: {e}")
+                    else:
+                        print(f"{data.strip()}")
+                        time.sleep(0.1)
+                        ser.write((response + "\r\n").encode())
+                        print(f"{response}")
+    except serial.SerialException as e:
+        print(f"Erro serial: {e}")
+    except KeyboardInterrupt:
+        os.system('cls')
+        print("\nSimulador encerrado.")
+
+if __name__ == "__main__":
+    simulate_esp()
