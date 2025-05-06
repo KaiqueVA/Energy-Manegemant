9d046057c91355205121d8c34a5d5cb28d86b2cc
nathaliaatamanchuk <nathaliaatamanchuk5@gmail.com>
2025-05-05 22:49:12 -0300

    feat: add UUID to Integer mapping methods in MeasurementMapper

--- Backend/power-manager-api/src/main/java/com/engcomp/power/power_manager_api/mapper/MeasurementMapper.java
+++ Backend/power-manager-api/src/main/java/com/engcomp/power/power_manager_api/mapper/MeasurementMapper.java
@@ -4,8 +4,18 @@ import com.engcomp.power.power_manager_api.domain.measurement.Measurement;
 import com.engcomp.power.power_manager_api.dto.MeasurementDTO;
 import org.mapstruct.Mapper;
 
+import java.util.UUID;
+
 @Mapper(componentModel = "spring")
 public interface MeasurementMapper {
+
     Measurement toEntity(MeasurementDTO dto);
     MeasurementDTO toDTO(Measurement entity);
+
+    default Integer map(UUID value) {
+        return value != null ? value.hashCode() : null;
+    }
+    default UUID map(Integer value) {
+        return value != null ? UUID.nameUUIDFromBytes(value.toString().getBytes()) : null;
+    }
 }
