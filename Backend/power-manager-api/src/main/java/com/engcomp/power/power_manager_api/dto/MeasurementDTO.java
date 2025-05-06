123aeb62713317107390f4953a5eb95ab0a031d1
nathaliaatamanchuk <nathaliaatamanchuk5@gmail.com>
2025-05-05 22:48:48 -0300

    feat: link measurements to user by user_id and use Integer id

--- Backend/power-manager-api/src/main/java/com/engcomp/power/power_manager_api/dto/MeasurementDTO.java
+++ Backend/power-manager-api/src/main/java/com/engcomp/power/power_manager_api/dto/MeasurementDTO.java
@@ -5,11 +5,13 @@ import java.util.UUID;
 
 @Data
 public class MeasurementDTO {
-    private UUID id;
+    private Integer id;
     private Double v_eficaz;
     private Double i_eficaz;
     private Double p_aparente;
     private Double p_ativa;
     private Double p_reativa;
     private Double fator_potencia;
+
+    private UUID user_id;
 }
