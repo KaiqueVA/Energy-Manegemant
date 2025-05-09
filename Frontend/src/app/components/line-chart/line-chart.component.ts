abc31c59b8b2349283a45e7f05212b4a0f66e475
gpelincel <gabrielpelincel@gmail.com>
2025-05-09 10:33:22 -0300

    Feat: Line chart no dashboard

--- Frontend/src/app/components/line-chart/line-chart.component.ts
+++ Frontend/src/app/components/line-chart/line-chart.component.ts
@@ -1,12 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, plugins } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-line-chart',
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.scss'],
 })
-
 export class LineChartComponent {
+  labels = ['a', 'b', 'c', 'd', 'f', 'g'];
 
+  cfg: any = {
+    type: 'line',
+    data: {
+      labels: this.labels,
+      datasets: [
+        {
+          color: '#fff',
+          data: [10, 20, 30, 25, 21, 29, 40],
+          label: 'Consumo em kWh',
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          labels:{
+            color: '#fff',
+          }
+        }
+      },
+      scales: {
+        x:{
+          grid: {
+            color: '#888',
+          },
+        },
+        y:{
+          grid: {
+            color: '#888',
+          },
+        }
+      },
+    },
+  };
 }
