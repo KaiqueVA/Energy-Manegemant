19e208f49395ad6a4b227029616e371067b3c419
KaiqueVA <kaique_x_@hotmail.com>
2025-06-05 18:50:01 -0300

    Feat: Adiciona controle de estado do relé

--- Backend/power-manager-api/src/main/java/com/engcomp/power/power_manager_api/controllers/RelayController.java
+++ Backend/power-manager-api/src/main/java/com/engcomp/power/power_manager_api/controllers/RelayController.java
@@ -9,17 +9,24 @@ import java.util.Map;
 @RequestMapping("/relay")
 public class RelayController {
 
+    private int currentState; // Assuming relay is off by default
     @PostMapping("/on")
     public ResponseEntity<Map<String, Integer>> turnOnRelay() {
         System.out.println("Relé LIGADO");
-
+        currentState = 1; // Update the state to ON
         return ResponseEntity.ok(Map.of("relay", 1));
     }
 
     @PostMapping("/off")
     public ResponseEntity<Map<String, Integer>> turnOffRelay() {
         System.out.println("Relé DESLIGADO");
-
+        currentState = 0; // Update the state to OFF
         return ResponseEntity.ok(Map.of("relay", 0));
+    }
+
+    @GetMapping("/state")
+    public ResponseEntity<Map<String, Integer>> getRelayState()
+    {
+        return ResponseEntity.ok(Map.of("relay", currentState)); // Assuming relay is off by default
     }
 }
